e307ce34d13e71540c2b2fa7ce89d842275aa240
Roywaller <roywaller@126.com>
2025-04-21 13:17:03 +0800

    Update script.py

--- script.py
+++ script.py
@@ -7,22 +7,24 @@ def get_content(date):
     try:
         response = requests.get(url)
         if response.status_code == 200 and response.text.strip():
-            return response.text
+            return url, response.text
     except requests.RequestException:
         pass
-    return None
+    return None, None
 
 
 def main():
     current_date = datetime.now()
-    for _ in range(7):  # 最多尝试 7 天
-        content = get_content(current_date)
+    for _ in range(7):
+        url, content = get_content(current_date)
         if content:
             with open('clash.yaml', 'w', encoding='utf-8') as file:
                 file.write(content)
+            print(url)
             break
         current_date -= timedelta(days=1)
 
 
 if __name__ == "__main__":
     main()
+    
